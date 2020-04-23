43092fc753f032020d0615c51b3211013fa31b6b
miguelassan001 <59732784+miguelassan001@users.noreply.github.com>
2020-04-23 09:47:34 +0000

    (pre) jue abr 23 09:47:34 UTC 2020

--- iac/output.tf
+++ iac/output.tf
@@ -1,4 +1,8 @@
-output "fichero" {
+output "file_content" {
   value       = local_file.fichero.content
   description = "File content"
+}
+output "file_name" {
+  value       = local_file.fichero.filename
+  description = "File name"
 }
